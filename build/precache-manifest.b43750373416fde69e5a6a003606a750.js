606fcb9d0e488902f74f919539d3b38a3ff4c7f9
Adam Kukliński <64474051+Q-Click@users.noreply.github.com>
2020-10-31 14:46:47 +0100

    Firebase deployment

---
+++ build/precache-manifest.b43750373416fde69e5a6a003606a750.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82f772b73dc29b36b1a01e02d7b97d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4db118287eb1d69be4",
+    "url": "/static/css/main.7c1f4c17.chunk.css"
+  },
+  {
+    "revision": "bf4a539d380191b17142",
+    "url": "/static/js/2.2d2a0fdb.chunk.js"
+  },
+  {
+    "revision": "ab4db118287eb1d69be4",
+    "url": "/static/js/main.aa1b8c95.chunk.js"
+  },
+  {
+    "revision": "2105b08a87ddb80017d5",
+    "url": "/static/js/runtime-main.5ff1357b.js"
+  }
+]);
